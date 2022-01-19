0037a2493735c539a7d63e8b95c0ca586a58441b
DevPrajapati-1409 <devprajapati1409@gmail.com>
2022-01-19 12:46:31 +0530

    Login and Logout with MetaMask completed

---
+++ components/Login.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+
+function Login() {
+  const { authenticate } = useMoralis();
+  return (
+    <div className="bg-black relative ">
+      <h1>I am the login</h1>
+
+      <div className="flex flex-col absolute z-50 h-1/2 w-full items-center justify-center space-y-6">
+        <Image
+          className="object-cover rounded-full bg-yellow-500"
+          src="https://cdn.dribbble.com/users/2973/screenshots/16757947/media/c56c56d24f8c591ff753faa15ea4a033.png?compress=1&resize=400x300"
+          width={240}
+          height={300}
+        />
+
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold hover:animate-pulse"
+        >
+          Login to the METAVERSE
+        </button>
+      </div>
+
+      <div className="w-full h-screen">
+        <Image
+          src="https://links.papareact.com/55n"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "avatars.dicebear.com",
+      "links.papareact.com",
+      "cdn.dribbble.com",
+    ],
+  },
+};
